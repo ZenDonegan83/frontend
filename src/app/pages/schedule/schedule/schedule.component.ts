2eb7f40cf9b34405d5380dd3cd121e5e323ca16d
ZenDonegan83 <zendonegan@gmail.com>
2023-02-04 19:27:05 +0000

    Fix: routing issue

--- src/app/pages/schedule/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule/schedule.component.ts
@@ -31,7 +31,7 @@ export class ScheduleComponent implements OnInit {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
     },
-    initialView: 'dayGridMonth',
+    initialView: 'timeGridWeek',
     weekends: true,
     editable: true,
     selectable: false,
